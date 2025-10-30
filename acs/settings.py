caa079123b9376e91192bdf3d32a16f2130f48f6
unknown <hbruancci@gmail.com>
2025-10-30 01:13:32 -0300

    ADD settings

--- acs/settings.py
+++ acs/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-change-me")
 
-DEBUG = os.getenv("DJANGO_DEBUG", "0") == "1"
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
 
