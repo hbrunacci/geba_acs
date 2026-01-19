edcf0a20817e642238ec7756519bc5ec05bed087
Hernan <hbrunacci@gmail.com>
2026-01-19 05:43:58 -0300

    Add events console UI

--- institutions/urls.py
+++ institutions/urls.py
@@ -1,6 +1,13 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import AccessDeviceViewSet, AccessPointViewSet, EventViewSet, SiteViewSet
+from .views import (
+    AccessDeviceViewSet,
+    AccessPointViewSet,
+    EventViewSet,
+    SiteViewSet,
+    events_console,
+)
 
 router = DefaultRouter()
 router.register(r"sites", SiteViewSet)
@@ -8,4 +15,6 @@ router.register(r"access-points", AccessPointViewSet)
 router.register(r"access-devices", AccessDeviceViewSet)
 router.register(r"events", EventViewSet)
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path("events-console/", events_console, name="events_console"),
+]
