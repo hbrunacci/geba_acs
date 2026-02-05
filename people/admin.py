d2ace8ed6df809fc7802f45d98da0b8344f1ba3f
Hernan <hbrunacci@gmail.com>
2026-02-04 22:38:44 -0300

    Add Cliente model

--- people/admin.py
+++ people/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import GuestInvitation, Person
+from .models import Cliente, GuestInvitation, Person
 
 
 @admin.register(Person)
@@ -22,3 +22,10 @@ class GuestInvitationAdmin(admin.ModelAdmin):
     list_display = ("person", "event", "guest_type", "created_at")
     search_fields = ("person__last_name", "person__first_name", "event__name")
     list_filter = ("guest_type", "event__site")
+
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("id_cliente", "razon_social", "apellido", "nombre", "email", "activo")
+    search_fields = ("id_cliente", "razon_social", "apellido", "nombre", "cuit", "email")
+    list_filter = ("activo", "id_estado_cliente", "mercado")
